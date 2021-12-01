ca89833ac2b1fac26406eb74dc97341c8a0fcf17
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2021-12-01 20:08:18 +0500

    Update LoginScenarios.java

--- src/main/java/com/login/LoginScenarios.java
+++ src/main/java/com/login/LoginScenarios.java
@@ -1,119 +1,126 @@
 package com.login;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.AssertJUnit;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.aventstack.extentreports.Status;
 import com.extent.report.ExtentReportBaseClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginScenarios extends ExtentReportBaseClass  {
+public class LoginScenarios extends ExtentReportBaseClass {
 
-	
-	public static ChromeDriver driver;	 
-	
-	
-	@Test
-    public void WrongPasswordTest() {
-	test = extent.createTest("WrongPasswordTest", "Testing login with wrong password"); 
-	WebDriverManager.chromedriver().setup();
-	driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://nstp-slmfrontend.evampsaanga.com/");
-	driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("rabail@managementportal.com");
-	driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("demo13499");
-	driver.findElementByXPath("//span[@class='MuiButton-label']").click();
-	test.log(Status.INFO, "Submitted form with wrong password");	
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	wait.until(ExpectedConditions.textToBePresentInElement(driver.findElementByXPath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/label[3]"),"Invalid user credentials"));
-	
-	AssertJUnit.assertEquals(driver.findElementByXPath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/label[3]").getText(), "Invalid user credentials");
-	driver.close();
-	
-    }
-	
-	@Test
-   	public void LoginEmptyTest() {
-	test = extent.createTest("LoginEmptyTest", "Testing login with empty fields"); 
-	WebDriverManager.chromedriver().setup();
-	driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://nstp-slmfrontend.evampsaanga.com/");
-	driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("");
-	driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("");
-	driver.findElementByXPath("//span[@class='MuiButton-label']").click();
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	wait.until(ExpectedConditions.textToBePresentInElement(driver.findElementByXPath("//*[@id=\'email-helper-text\']"),"Incorrect Username"));
-	wait.until(ExpectedConditions.textToBePresentInElement(driver.findElementByXPath("//*[@id=\'password-helper-text\']"),"Password is required and must not be less than 4 characters"));
-	AssertJUnit.assertEquals(driver.findElementById("email-helper-text").getText(), "Incorrect Username");	
-	AssertJUnit.assertEquals(driver.findElementById("password-helper-text").getText(), "Password is required and must not be less than 4 characters");	
-	driver.close();
-		
-	     }
-	
+	// rabail@managementportal.com
+	// demo1234
+
+	public static ChromeDriver driver;
 
 	@Test
-   	public void LoginTest() {
-	test = extent.createTest("LoginTest", "Testing login with valid credentials"); 
-	WebDriverManager.chromedriver().setup();
-	driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://nstp-slmfrontend.evampsaanga.com/");
-	driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("rabail@managementportal.com");
-	driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("demo1234");
-	driver.findElementByXPath("//span[@class='MuiButton-label']").click();
-	WebDriverWait wait = new WebDriverWait(driver, 20);
-	wait.until(ExpectedConditions.urlContains("dashboard"));
-	AssertJUnit.assertEquals(driver.getCurrentUrl(), "https://nstp-slmfrontend.evampsaanga.com/#/admin-dashboard");
-	driver.close();
-		
-	     }
-	
-	
-   	public static void LandConfigTest() {
-   		
-   	WebDriverManager.chromedriver().setup();
-   	driver=new ChromeDriver();
-   	driver.manage().window().maximize();
-	driver.get("https://nstp-slmfrontend.evampsaanga.com/#/sign-in");
-	driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("rabail@managementportal.com");
-	driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("demo1234");
-	driver.findElementByXPath("//span[@class='MuiButton-label']").click();
-	WebDriverWait wait = new WebDriverWait(driver, 30);
-	wait.until(ExpectedConditions.urlContains("dashboard"));
-	driver.findElementByXPath("//*[@id=\"root\"]/div[2]/div[1]/div/div/h4").click();
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//main/h2")));
-	if(driver.findElementByXPath("//span[text()='Dashboard']").isEnabled())
-		Reporter.log("Landed on dashboard");
-	else  
-		Reporter.log("Error while Landing on dashboard");
-	//	driver.close();
-		
-	     }
-	
-   
+	public void WrongPasswordTest() {
+		SoftAssert sf = new SoftAssert();
+		test = extent.createTest("WrongPasswordTest", "Testing login with wrong password");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://nstp-slmfrontend.evampsaanga.com/");
+		driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("ramzan@developer.com");
+		driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("admin1");
+		driver.findElementByXPath("//span[@class='MuiButton-label']").click();
+		test.log(Status.INFO, "Submitted form with wrong password");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.textToBePresentInElement(
+				driver.findElementByXPath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/label[3]"),
+				"Invalid username or password"));
+
+		sf.assertEquals(driver.findElementByXPath("//*[@id=\"root\"]/main/div/div/div/div[2]/div/label[3]").getText(),
+				"Invalid username or password");
+		driver.close();
+
+	}
+
 	@Test
-   	public void LogoutTest() {
-		test = extent.createTest("LogoutTest", "Testing Logout"); 
+	public void LoginEmptyTest() {
+		test = extent.createTest("LoginEmptyTest", "Testing login with empty fields");
+		SoftAssert sf = new SoftAssert();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://nstp-slmfrontend.evampsaanga.com/");
+		driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("");
+		driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("");
+		driver.findElementByXPath("//span[@class='MuiButton-label']").click();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.textToBePresentInElement(
+				driver.findElementByXPath("//*[@id=\'email-helper-text\']"), "Incorrect Username"));
+		wait.until(ExpectedConditions.textToBePresentInElement(
+				driver.findElementByXPath("//*[@id=\'password-helper-text\']"),
+				"Password is required and must not be less than 4 characters"));
+		sf.assertEquals(driver.findElementById("email-helper-text").getText(), "Incorrect Username");
+		sf.assertEquals(driver.findElementById("password-helper-text").getText(),
+				"Password is required and must not be less than 4 characters");
+		driver.close();
+
+	}
+
+	@Test
+	public void LoginTest() {
+		SoftAssert sf = new SoftAssert();
+		test = extent.createTest("LoginTest", "Testing login with valid credentials");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://nstp-slmfrontend.evampsaanga.com/");
+		driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("ramzan@developer.com");
+		driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("admin");
+		driver.findElementByXPath("//span[@class='MuiButton-label']").click();
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.urlContains("dashboard"));
+		sf.assertEquals(driver.getCurrentUrl(), "https://nstp-slmfrontend.evampsaanga.com/#/admin-dashboard");
+		driver.close();
+
+	}
+
+	public static void LandConfigTest() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://nstp-slmfrontend.evampsaanga.com/#/sign-in");
+		driver.findElementByXPath("//*[@id=\"email\"]").sendKeys("ramzan@developer.com");
+		driver.findElementByXPath("//*[@id=\"password\"]").sendKeys("admin");
+		driver.findElementByXPath("//span[@class='MuiButton-label']").click();
+		WebDriverWait wait = new WebDriverWait(driver, 100);
+		wait.until(ExpectedConditions.urlContains("dashboard"));
+		driver.findElementByXPath("//*[@id=\"root\"]/div[2]/div[1]/div/div/h4").click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//main/h2")));
+		if (driver.findElementByXPath("//span[text()='Dashboard']").isEnabled())
+			Reporter.log("Landed on dashboard");
+		else
+			Reporter.log("Error while Landing on dashboard");
+		// driver.close();
+
+	}
+	
+	@Test
+	public void LogoutTest() {
+		test = extent.createTest("LogoutTest", "Testing Logout");
 		LandConfigTest();
 		WebDriverWait wait = new WebDriverWait(driver, 20);
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElementByClassName("MuiSvgIcon-root")));
-	//	driver.findElementByClassName("MuiSvgIcon-root").click();
-		driver.findElementByCssSelector("#root > div.makeStyles-root-5.makeStyles-shiftContent-6 > header > div > svg").click();
-		wait.until(ExpectedConditions.elementToBeClickable(driver.findElementByXPath("/html/body/div[2]/div[3]/div/div[3]/button[2]")));
-	    driver.findElementByXPath("/html/body/div[2]/div[3]/div/div[3]/button[2]").click();
-	    driver.close();
+		driver.findElementByCssSelector("#root > div.jss5.jss6 > header > div > svg:nth-child(5)").click();
+		// #root > div.jss5.jss6 > header > div > svg:nth-child(5)
+		// #root > div.makeStyles-root-5.makeStyles-shiftContent-6 > header > div > svg
+		wait.until(ExpectedConditions
+				.elementToBeClickable(driver.findElementByXPath("/html/body/div[2]/div[3]/div/div[3]/button[2]")));
+		driver.findElementByXPath("/html/body/div[2]/div[3]/div/div[3]/button[2]").click();
+		// //span[contains(.,'Yes')]
+		driver.close();
 		driver.quit();
-	
-		
-	     }
-	
-	
-	
+
+	}
 
 }
